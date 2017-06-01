810795cfa14d2c75da2ecf8ea476304895bb0cde
Murillo <msgrubler@gmail.com>
2017-05-31 23:51:41 -0300

    challenge Diagonal Difference added

--- Warmup/diagonal-difference.py
+++ Warmup/diagonal-difference.py
@@ -1,12 +1,17 @@
 # Diagonal Difference
 # Developer: Murillo Grubler
 
-#!/bin/python3
-
-import sys
-
 n = int(input().strip())
 a = []
+value_left = 0
+value_right = 0
+left_index = 0
+right_index = n - 1
 for a_i in range(n):
     a_t = [int(a_temp) for a_temp in input().strip().split(' ')]
-    a.append(a_t)
+    value_left  = value_left + a_t[left_index]
+    value_right = value_right + a_t[right_index]
+    left_index  += 1
+    right_index -= 1
+
+print (abs(value_left - value_right))
