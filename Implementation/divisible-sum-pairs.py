51a29d677dee793817f0f15f95397203f6133935
Murillo Grubler <msgrubler@gmail.com>
2017-07-24 19:47:07 -0300

    start challenge divisible sum pairs

---
+++ Implementation/divisible-sum-pairs.py
@@ -0,0 +1,11 @@
+# Divisible Sum Pairs
+# Developer: Murillo Grubler
+
+def divisibleSumPairs(n, k, ar):
+    # Complete this function
+    return 1
+
+n, k = [int(numbers) for numbers in input().strip().split(' ')]
+ar = list(map(int, input().strip().split(' ')))
+result = divisibleSumPairs(n, k, ar)
+print(result)
