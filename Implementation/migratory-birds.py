19b3e83739f81eaecb8a3bd55cba1963a459fe12
Murillo Grubler <msgrubler@gmail.com>
2017-07-31 22:58:41 -0300

    Added challenge Migratory Birds

---
+++ Implementation/migratory-birds.py
@@ -0,0 +1,30 @@
+# Migratory Birds
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/migratory-birds/problem
+
+def amount(ar):
+    types = {}
+    for i in range(len(ar)):
+        if ar[i] in types:
+            types[ar[i]] += 1
+        else:
+            types[ar[i]] = 1
+    return types
+
+def migratoryBirds(n, ar):
+    all_types = amount(ar)
+    types = 0
+    quantity = 0
+    for key, value in all_types.items():
+        if value == quantity and types > key:
+            types = key
+            quantity = value
+        elif value > quantity and types != key:
+            types = key
+            quantity = value
+    return types 
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = migratoryBirds(n, ar)
+print(result)
