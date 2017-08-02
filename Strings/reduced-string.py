e4809c4d494d779257c4faeb3b87493547947835
Murillo Grubler <msgrubler@gmail.com>
2017-08-01 23:13:31 -0300

    start challenge Super Reduced String

---
+++ Strings/reduced-string.py
@@ -0,0 +1,10 @@
+# Super Reduced String
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/reduced-string
+
+def super_reduced_string(s):
+    # Complete this function
+    return ""
+
+s = input().strip()
+print(super_reduced_string(s))
