4c449a55a10e5d55e2c2705fbe284a766cb8bd60
Murillo Grubler <msgrubler@gmail.com>
2017-08-03 22:03:37 -0300

    challenge Super Reduced String

--- Strings/reduced-string.py
+++ Strings/reduced-string.py
@@ -3,18 +3,16 @@
 # Link: https://www.hackerrank.com/challenges/reduced-string
 
 def remove_pair_item(ar):
-    remove_any_char = False
     last_string = ""
-    for i in range((len(ar) - 1), 0, -1):
+    total = len(ar) - 1
+    for i in range(total, -1, -1):
         if last_string == ar[i]:
             ar.pop(i + 1)
             ar.pop(i)
-            remove_any_char = True
-            #last_string = ""
-            break
+            return True, ar
         else:
             last_string = ar[i]
-    return remove_any_char, ar
+    return False, ar
 
 def super_reduced_string(ar):
     if len(ar) > 0:
@@ -23,12 +21,10 @@ def super_reduced_string(ar):
         removed_char = True
         while (removed_char):
             removed_char, chars = remove_pair_item(chars)
-            if removed_char:
-                count += 1
-        if count > 0:
+        if len(chars) > 0:
             return chars
     return "Empty String"
     
 s = list(input().strip())
-print(super_reduced_string(s))
-#print ("".join(str(x) for x in s))
+result = super_reduced_string(s)
+print ("".join(str(x) for x in result))
