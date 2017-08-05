7cb61aa7add5084037602641f560341d61a0eea5
Murillo Grubler <msgrubler@gmail.com>
2017-08-05 18:23:19 -0300

    improve code (faster)

--- Strings/two-characters.py
+++ Strings/two-characters.py
@@ -10,15 +10,20 @@ def validate(text):
 
 s_len = int(input().strip())
 s = input().strip()
+black_list = []
 if s_len > 1:
     sl = list(s)
     max_len = 0
     for x in range(len(sl)):
         for y in range(x+1, len(sl)):
-            if sl[x] != sl[y]:
+            if sl[x] != sl[y] and (sl[x] not in black_list or sl[y] not in black_list):
                 text = [c for c in s if c == sl[x] or c == sl[y]]
                 if validate(text):
                     max_len = max(max_len, len(text))
+            elif sl[x] not in black_list:
+                black_list.append(sl[x])
+            elif sl[y] not in black_list:
+                black_list.append(sl[y])
     print (max_len)
 else:
     print (0)
