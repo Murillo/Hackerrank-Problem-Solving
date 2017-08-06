445fda21c36bb10e25c622e98bb5d737de9f2e73
Murillo Grubler <msgrubler@gmail.com>
2017-08-06 20:48:07 -0300

    I added another sort types in code

--- Sorting/big-sorting.py
+++ Sorting/big-sorting.py
@@ -2,6 +2,15 @@
 # Developer: Murillo Grubler
 # Link: https://www.hackerrank.com/challenges/big-sorting/problem
 
+def bubble_sort(arr):
+    for passnum in range(len(arr)-1,0,-1):
+        for i in range(passnum):
+            if arr[i] > arr[i+1]:
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+    return arr
+
 def insertion_sort(arr):
     for i in range(1, len(arr)):
         key = arr[i]
@@ -18,6 +27,6 @@ for unsorted_i in range(n):
    unsorted_t = int(input().strip())
    unsorted.append(unsorted_t)
 
-unsorted = insertion_sort(unsorted)
+unsorted = bubble_sort(unsorted)
 for i in range(len(unsorted)):
     print (unsorted[i])
