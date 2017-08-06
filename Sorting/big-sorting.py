376b5168fd618e0d5b7e3acd712de8dbfe26fb7c
Murillo Grubler <msgrubler@gmail.com>
2017-08-06 20:23:47 -0300

    started challenge big sorting

--- Sorting/big-sorting.py
+++ Sorting/big-sorting.py
@@ -2,10 +2,22 @@
 # Developer: Murillo Grubler
 # Link: https://www.hackerrank.com/challenges/big-sorting/problem
 
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        k = i
+        while k > 0 and key < arr[k - 1]:
+            arr[k] = arr[k - 1]
+            k -= 1
+        arr[k] = key
+    return arr
+
 n = int(input().strip())
 unsorted = []
-unsorted_i = 0
 for unsorted_i in range(n):
-   unsorted_t = str(input().strip())
+   unsorted_t = int(input().strip())
    unsorted.append(unsorted_t)
-# your code goes here
+
+unsorted = insertion_sort(unsorted)
+for i in range(len(unsorted)):
+    print (unsorted[i])
