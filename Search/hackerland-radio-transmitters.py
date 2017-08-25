a94f9cada9442938a4f5f8f9c489af0d24dcff9e
Murillo Grubler <msgrubler@gmail.com>
2017-08-24 22:17:26 -0300

    update code "Hackerland Radio Transmitters" challenge

--- Search/hackerland-radio-transmitters.py
+++ Search/hackerland-radio-transmitters.py
@@ -4,23 +4,22 @@
 
 # n,k = [int(points) for points in input().strip().split(' ')]
 # x   = sorted([int(x_temp) for x_temp in input().strip().split(' ')])
-n, k = 5, 1
-x = [1, 2, 3, 4, 5]
+n, k = 8, 2
+x = [7, 2, 4, 6, 5, 9, 12, 11]
 area_k = (k * 2) + 1
-count_area = 0
+count_area = 1
 radios = 0
 
 for i in range(min(x), max(x) + 1):
-    print (i)
+    print (count_area)
     if count_area == int(area_k / 2):
         radios += 1
-        print ("radio na casa {}".format(i))
+        print ("count radio {} na casa {}".format(count_area, i))
+    elif ((max(x) - i) / area_k) < 1:
+        radios += 1
+        print ("count radio {} na casa {}".format(count_area, i))
+        break
     elif count_area == area_k:
         count_area = 0
-        if (max(x) - i) < area_k:
-            radios += 1
-            print ("radio na casa {}".format(i))
-
     count_area += 1
-
 print (radios)
