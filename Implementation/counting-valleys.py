792ecfd560f732b6fda6bf45139bc3084ccadc93
Murillo Grubler <msgrubler@gmail.com>
2017-08-29 21:22:27 -0300

    added challenge "Counting Valleys"

---
+++ Implementation/counting-valleys.py
@@ -0,0 +1,20 @@
+# Counting Valleys
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/counting-valleys/problem
+
+def counting_valleys(n, ar):
+    level = 0
+    sea_level = False
+    count_down_level = 0
+    for i in range(n):
+        level = level + 1 if ar[i] == "U" else level - 1
+        if not sea_level and level < 0:
+            sea_level = True
+            count_down_level += 1
+        elif sea_level and level >= 0:
+            sea_level = False
+    return count_down_level
+
+n = int(input().strip())
+ar = input().strip()
+print (counting_valleys(n, ar))
