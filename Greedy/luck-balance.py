efff7b03b4b683818561dd637d8b4e045933362e
Murillo Grubler <msgrubler@gmail.com>
2017-09-02 21:07:29 -0300

    added challenge "Luck Balance"

--- Greedy/luck-balance.py
+++ Greedy/luck-balance.py
@@ -4,19 +4,15 @@
 
 n, k =  [int(val) for val in input().strip().split(' ')]
 luck = 0
-no_luck = 0
-importants = 0
+imp_values = []
 for i in range(n):
     L, T =  [int(val) for val in input().strip().split(' ')]
-    if T == 1:
-        importants += 1
-    if importants < k:
+    if T == 0:
         luck += L
     else:
-        importants = 0
-        no_luck += L
-    
-
-print (no_luck)
-print (luck)
-print (luck - no_luck)
+        imp_values.append(L) 
+imp_values.sort(key=int)
+for i in range(k):
+    if len(imp_values) > 0:
+        luck += imp_values.pop()
+print (luck - sum(imp_values))
