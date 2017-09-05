ef8e2eca757b19840dfa5d597eb777a940b69771
Murillo Grubler <msgrubler@gmail.com>
2017-09-04 23:22:26 -0300

    added challenge "Cats and a Mouse"

---
+++ Implementation/cats-and-a-mouse.py
@@ -0,0 +1,21 @@
+# Cats and a Mouse
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/cats-and-a-mouse/problem
+# big-O notation = O(1)
+
+import math
+
+def catch_mouse(x, y, z):
+    cat_1 = math.sqrt((x - z) ** 2)
+    cat_2 = math.sqrt((y - z) ** 2)
+    if cat_1 < cat_2:
+        return "Cat A"
+    elif cat_2 < cat_1:
+        return "Cat B"
+    else:    
+        return "Mouse C"
+
+q = int(input().strip())
+for a0 in range(q):
+    x, y, z =  [int(points) for points in input().strip().split(' ')]
+    print (catch_mouse(x, y, z))
