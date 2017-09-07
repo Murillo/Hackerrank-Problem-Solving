0dc3306e8dc2857e7e9483f174a8d242470d979e
Murillo Grubler <msgrubler@gmail.com>
2017-09-07 14:06:32 -0300

    added challenge "Designer PDF Viewer"

--- Implementation/designer-pdf-viewer.py
+++ Implementation/designer-pdf-viewer.py
@@ -1,6 +1,13 @@
 # Designer PDF Viewer
 # Developer: Murillo Grubler
 # Link: https://www.hackerrank.com/challenges/designer-pdf-viewer/problem
+# Big O = O(n)
+
+def area_highlighted(h_words, word):
+    letters = [ord(i.lower()) - ord('a') for i in word]
+    area = max([h_words[i] for i in letters]) * len(letters)
+    return area
 
 h = list(map(int, input().strip().split(' ')))
 word = input().strip()
+print (area_highlighted(h, word))
