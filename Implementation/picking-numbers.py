b87c95948d7aa9ef07cce8e799d26aeb185f572d
Murillo Grubler <msgrubler@gmail.com>
2017-09-12 21:52:19 -0300

    "Picking Numbers" challenge finalized

--- Implementation/picking-numbers.py
+++ Implementation/picking-numbers.py
@@ -2,24 +2,14 @@
 # Developer: Murillo Grubler
 # Link: https://www.hackerrank.com/challenges/picking-numbers/problem
 
-from collections import Counter
-
 def picking_number(n, arr):
-    numbers = []
+    max_combinations = 0
     for i in range(n):
-        composition = [arr[i]]
-        for j in range(n):
-            if  i != j and arr[i] - arr[j] <= 1 and arr[i] - arr[j] >= -1:
-                composition.append(arr[j])
-        if len(composition) > len(numbers):
-            numbers = composition
-
-    # print (numbers)
-    min_num = numbers.count(min(numbers)) + numbers.count(min(numbers) + 1)
-    max_num = numbers.count(max(numbers)) + numbers.count(max(numbers) - 1)
-    return min_num if min_num >= max_num else max_num
+        combination = arr.count(arr[i]) + arr.count(arr[i] + 1)
+        if combination > max_combinations: 
+            max_combinations = combination
+    return max_combinations
 
 n = int(input().strip())
 a = [int(a_temp) for a_temp in input().strip().split(' ')]
-print (a)
 print (picking_number(n, a))
