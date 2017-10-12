1ff4e2885bd49eaba395fe55b3f175ea5f9ccaa1
Murillo Grubler <msgrubler@gmail.com>
2017-10-12 19:29:35 -0300

    added challenge "Angry Professor"

---
+++ Implementation/angry-professor.py
@@ -0,0 +1,15 @@
+# Angry Professor
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/angry-professor/problem
+
+t = int(input().strip())
+for a0 in range(t):
+    n,k = list(map(int, input().strip().split(' ')))
+    a = [int(a_temp) for a_temp in input().strip().split(' ')]
+    early = 0
+
+    for i in range(n):
+        if a[i] <= 0:
+            early += 1
+    
+    print ("NO" if early >= k else "YES")
