057124131e4589692c0ab526e0668f651c755db0
Murillo Grubler <msgrubler@gmail.com>
2017-10-12 19:34:14 -0300

    update code

--- Implementation/angry-professor.py
+++ Implementation/angry-professor.py
@@ -1,13 +1,13 @@
 # Angry Professor
 # Developer: Murillo Grubler
 # Link: https://www.hackerrank.com/challenges/angry-professor/problem
+# Time complexity: O(n²)
 
 t = int(input().strip())
 for a0 in range(t):
     n,k = list(map(int, input().strip().split(' ')))
     a = [int(a_temp) for a_temp in input().strip().split(' ')]
     early = 0
-
     for i in range(n):
         if a[i] <= 0:
             early += 1
