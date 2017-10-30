792abb6674ada1b207effe362b1a906f56012bf2
Murillo Grubler <msgrubler@gmail.com>
2017-10-29 22:39:32 -0200

    Repeated String in development

--- Implementation/repeated-string.py
+++ Implementation/repeated-string.py
@@ -3,6 +3,8 @@
 # https://www.hackerrank.com/challenges/repeated-string/problem
 # Time Complexity = O(n)
 
+import math 
+
 def repeated_string(text, total):
     if len(text) == 1 and text == 'a':
         return total
@@ -10,19 +12,7 @@ def repeated_string(text, total):
     add_value = 0
     if text.find('a')!=-1:
         add_value = len([text[i] for i in range(len(text)) if text[i] == 'a'])
-    amount_str = 0
-    new_text = ""
-    for i in range(total):
-        if len(new_text) < total:
-            new_text += text
-            if add_value > 0:
-                amount_str += add_value
-        else:
-            diff = (len(new_text) - total) - 1
-            text_out = new_text[(total - diff):total]
-            amount_str -= len([text_out[i] for i in range(len(text_out)) if text_out[i] == 'a'])
-            new_text = new_text[:total]
-            break
+    amount_str = math.ceil((total / len(text)) * add_value)
     return amount_str
 
 s = input().strip()
