4dcd74505a839aedbbe9fadd78e56382399f30c8
Murillo <msgrubler@gmail.com>
2017-11-27 20:26:34 -0200

    Added challenge 'Mark and Toys' finalized

---
+++ Greedy/mark-and-toys.py
@@ -0,0 +1,19 @@
+# Mark and Toys
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/mark-and-toys/problem
+
+def maximumToys(prices, k):
+    total = 0
+    prices.sort()
+    for i in range(len(prices)):
+        k = k - prices[i]
+        if k >= 0:
+            total += 1
+        else:
+            break 
+    return total
+
+n, k = map(int, input().strip().split(' '))
+prices = list(map(int, input().strip().split(' ')))
+result = maximumToys(prices, k)
+print(result)
