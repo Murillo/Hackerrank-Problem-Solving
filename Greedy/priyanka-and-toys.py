9d851d8fbd42570b3f9c43d1190cebd0b24dcd45
Murillo Grubler <msgrubler@gmail.com>
2018-01-21 13:14:59 -0200

    challenge 'Priyanka and Toys' finalized

---
+++ Greedy/priyanka-and-toys.py
@@ -0,0 +1,18 @@
+# Priyanka and Toys
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/priyanka-and-toys/problem
+# Time complexity: O(n)
+
+def toys(w):
+    w.sort()
+    total = 1
+    free_toys = w[0] + 4
+    for i in range(1, len(w)):
+        if w[i] > free_toys:
+            total += 1
+            free_toys = w[i] + 4
+    return total
+
+n = int(input().strip())
+w = list(map(int, input().strip().split(' ')))
+print(toys(w))
