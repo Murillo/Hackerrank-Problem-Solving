290dab16ee8860e92e4f6405de33dbe511bec657
Murillo <msgrubler@gmail.com>
2018-02-18 19:55:30 -0300

    challenge 'Funny String' finalized

--- Strings/funny-string.py
+++ Strings/funny-string.py
@@ -2,22 +2,19 @@
 # Developer: Murillo Grubler
 # Link: https://www.hackerrank.com/challenges/funny-string/problem
 
+# Import library
 import string
 
-# ca - xz
-# xc - cx
-# zx - ac
+# Time complexity: O(n)
+def funnyString(arr):
+    s = list([string.ascii_lowercase.index(i) for i in arr])
+    r = list([string.ascii_lowercase.index(i) for i in arr[::-1]])
+    for i in range(1, len(arr) - 1):
+        if abs(s[i] - s[i - 1]) != abs(r[i] - r[i - 1]):
+            return "Not Funny"
+    return "Funny"
 
-#acxz - 3
-#acxza
-
-def funnyString(s):
-    str_inverted = s[::-1]
-    for i in range(len(s) - 1):
-        print(s[i])
-
-    return ""
-
+# It starts the program calling the function funnyString
 q = int(input().strip())
 for a0 in range(q):
     print(funnyString(input().strip()))
