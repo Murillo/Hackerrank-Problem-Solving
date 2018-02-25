5c094f41a0b50b33c059d148a525edf00d85a1be
Murillo <msgrubler@gmail.com>
2018-02-24 22:51:55 -0300

    The challenge 'Alternating Characters'  was added

---
+++ Strings/alternating-characters.py
@@ -0,0 +1,17 @@
+# Alternating Characters
+# Developer: Murillo Grubler
+# Link: https://www.hackerrank.com/challenges/alternating-characters/problem
+
+def alternatingCharacters(s):
+    sumChars = 0
+    for i in range(len(s)):
+        if i == 0 or tempChar != s[i]:
+            tempChar = s[i]
+            continue
+        if tempChar == s[i]:
+            sumChars += 1
+    return sumChars
+
+q = int(input().strip())
+for a0 in range(q):
+    print(alternatingCharacters(input().strip()))
