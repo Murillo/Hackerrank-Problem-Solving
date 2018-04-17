39e77a991127276242eb5ccd4cd10df1d62bd971
Murillo Grubler <msgrubler@gmail.com>
2018-04-17 19:46:13 -0300

    It was added a new challenge called "The Coin Change Problem"

--- Dynamic Programming/coin-change.py
+++ Dynamic Programming/coin-change.py
@@ -1,16 +1,18 @@
 # The Coin Change Problem
-# Developer: Murillo Grubler
-# https://www.hackerrank.com/challenges/coin-change/problem
+# Developer: Murillo Grübler
+# Link: https://www.hackerrank.com/challenges/coin-change/problem
+# Time complexity: O(n²)
 
-# Não existe limite para a quantidade de caracteres
-# Saber se o total é par ou impar
-
-def getWays(n, c):
-    # Complete this function
-    return 0
+def getWays(arr, m, n):
+	table = [1 if i == 0 else 0 for i in range(n + 1)]
+	for i in range(m):
+		for j in range(1, len(table)):
+			result = j - arr[i]
+			if result >= 0:
+				table[j] = table[result] + table[j]
+	return table[len(table)-1]
 
 n, m = input().strip().split(' ')
 n, m = [int(n), int(m)]
-c = list(map(int, input().strip().split(' ')))
-# Print the number of ways of making change for 'n' units using coins having the values given by 'c'
-print (getWays(n, c))
+arr = list(map(int, input().strip().split(' ')))
+print(getWays(arr, m, n))
