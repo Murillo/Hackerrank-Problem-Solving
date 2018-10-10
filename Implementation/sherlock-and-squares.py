c9e0b97aff981de81020ea35ad23540ef2ccd40a
Murillo Grubler <msgrubler@gmail.com>
2018-10-09 22:46:41 -0300

    "Sherlock and Squares" has been finalized

--- Implementation/sherlock-and-squares.py
+++ Implementation/sherlock-and-squares.py
@@ -8,17 +8,14 @@ import os
 # Function
 ###################################################
 def squares(a, b):
-    k = 1
+    k = int(a ** (1/2))
     total = 0
     while k <= int(b/2):
-        for i in range(b):
-            if i > int(b/2):
-                break
-
-            result = k ** 2
-            if result >= a and result <= b:
-                total += 1
-                break
+        result = k ** 2
+        if result > b:
+            break
+        if result >= a and result <= b:
+            total += 1
         k += 1
     return total
 ####################################################
@@ -27,8 +24,7 @@ if __name__ == '__main__':
     # fptr = open(os.environ['OUTPUT_PATH'], 'w')
     for q_itr in range(int(input())):
         ab = input().split()
-        #result = squares(int(ab[0]), int(ab[1]))
-        result = squares(1, 1000)
-        print ("Total: " + str(result))
+        result = squares(int(ab[0]), int(ab[1]))
+        print (str(result))
         # fptr.write(str(result) + '\n')
     # fptr.close()
