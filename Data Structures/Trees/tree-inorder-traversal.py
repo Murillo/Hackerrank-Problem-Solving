90889d71f11633d686627571e648fd213fe6a68d
Murillo Grubler <msgrubler@gmail.com>
2019-10-31 21:42:02 -0300

    Tree: Inorder Traversal challenge finalized

---
+++ Data Structures/Trees/tree-inorder-traversal.py
@@ -0,0 +1,60 @@
+# Tree: Inorder Traversal
+# Developer: Murillo Grubler
+# https://www.hackerrank.com/challenges/tree-inorder-traversal/problem
+# Time complexity: O(n)
+
+class Node:
+    def __init__(self, info): 
+        self.info = info  
+        self.left = None  
+        self.right = None 
+        self.level = None 
+
+    def __str__(self):
+        return str(self.info) 
+
+class BinarySearchTree:
+    def __init__(self): 
+        self.root = None
+
+    def create(self, val):  
+        if self.root == None:
+            self.root = Node(val)
+        else:
+            current = self.root
+         
+            while True:
+                if val < current.info:
+                    if current.left:
+                        current = current.left
+                    else:
+                        current.left = Node(val)
+                        break
+                elif val > current.info:
+                    if current.right:
+                        current = current.right
+                    else:
+                        current.right = Node(val)
+                        break
+                else:
+                    break
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.info (the value of the node)
+"""
+def inOrder(root):
+    if root.left is not None:
+        inOrder(root.left)
+    print(root, end=" ")
+    if root.right is not None:
+        inOrder(root.right)
+
+tree = BinarySearchTree()
+t = int(input())
+arr = list(map(int, input().split()))
+for i in range(t):
+    tree.create(arr[i])
+inOrder(tree.root)
